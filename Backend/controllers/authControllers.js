b7d86ed69676fa9036343e41de380c2cea7ccc34
soumen0099 <janasoumen14554@gmail.com>
2026-04-06 23:29:21 +0530

    controller Setup starting

---
+++ Backend/controllers/authControllers.js
@@ -0,0 +1,13 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const registerUser = async (req, res) => {};
+
+
+export const loginUser = async (req, res) => {};
+
+
+export const profileUser = async (req, res) => {};
